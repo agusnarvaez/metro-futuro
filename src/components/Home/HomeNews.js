2585a9df7b7a67562eb9a52f2a553d6da1c22f11
agusnarvaez <agus.narvaez@outlook.com>
2022-07-09 21:07:26 -0300

    Implementación metodología BEM en Novedades de la Home

--- src/components/Home/HomeNews.js
+++ src/components/Home/HomeNews.js
@@ -5,14 +5,14 @@ function HomeNews() {
 
     return (
 
-        <section className="homeNews">
-                <div className="homeNewsText">
-                    <h3>Novedades</h3>
+        <section className="sectionInfo" id="homeNews">
+                <div id="homeNews_Text">
+                    <h2>Novedades</h2>
                     <p>Entérate de últimas noticias del sector inmobiliario en Europa y Latam.</p>
                 </div>
-                <div className="homeNewsLink">
-                    <div>
-                        <NavLink to='/conact'>
+                <div className="degradedLink" id="homeNews_degradedLink">
+                    <div className='degradedLink_TextContainer degradedLink_TextContainer--lightBlue'>
+                        <NavLink className="degradedLink_Text"to='/conact'>
                             Quiero saber más
                         </NavLink>
                     </div>
