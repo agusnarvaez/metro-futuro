9e0cc9285f3954a0c73ff03f2e0164aa4e771d34
agusnarvaez <agus.narvaez@outlook.com>
2022-07-16 23:58:48 -0300

    Implementación metodología BEM en estilos de  AboutInfo multiplataforma

--- src/components/About/AboutInfo.js
+++ src/components/About/AboutInfo.js
@@ -6,14 +6,14 @@ import aboutInfoBg from "../../assets/img/background/aboutInfoBg.png";
 function AboutInfo() {
 
     return (
-        <section className="aboutInfo">
-            <img alt="sectionBackground" className="sectionBackground" src={aboutInfoBg} />
-            <h2>Agregamos valor generando mayor acceso a la <font color="#004DFF">industria inmobiliaria</font></h2>
-            <p>
+        <section id="aboutInfo">
+            <h2 className='section_subTitle'>Agregamos valor generando mayor acceso a la <font color="#004DFF">industria inmobiliaria</font></h2>
+            <p className='section_paragraph'>
                 Un inmueble representa una necesidad primaria para cualquier persona, convirtiéndola así a la inversión rentable, productiva y segura.<br />
                 Utilizamos <b>tecnología blockchain</b> para registrar los tokens inmobiliarios a través de Smart Contracts (contratos inteligentes) permitiéndonos generar pequeñas participaciones del inmueble, lo que llamamos el fraccionamiento por m<sup>2</sup>, es decir, cualquierpersona puede comprar uin toquen que es una representación digital del negocio asociado al inmueble.<br />
                 Desde nuestra plataforma con tu billetera digital podes manejar tus inversiones, vender o comprar los tokens y recibir los dividendos mensualmente.
             </p>
+            <img alt="sectionBackground" className="sectionBackground" src={aboutInfoBg} />
         </section>
     )
 }
