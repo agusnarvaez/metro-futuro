c53f9ce4e196398f1cf6525b3934ebb46428c17c
agusnarvaez <agus.narvaez@outlook.com>
2022-08-24 02:44:09 -0300

    Corrección de DegradedLink

--- src/components/DegradedLink.js
+++ src/components/DegradedLink.js
@@ -1,19 +1,12 @@
 //* Importo React
 import React from "react";
-/* import {NavLink}from "react-router-dom"; */
-
 
 function DegradedLink(props) {
-    const scrollToTop = () =>{
-        window.scrollTo({
-          top: 0, 
-          behavior: 'smooth'
-        });
-      };
+    
 return (
-    <div onClick={scrollToTop} className="degradedLink" id={props.id}>
+    <div className="degradedLink" id={props.id}>
         <div className={`degradedLink_TextContainer degradedLink_TextContainer--${props.backgroundColor}`}>
-            <a href={props.route} className="degradedLink_Text">
+            <a href={props.route} className="degradedLink_Text" target="_blank" rel="noreferrer">
                 {props.text}
             </a>
         </div>
