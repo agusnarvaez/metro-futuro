893b2bf1dec82d61614df9b4bcccc15a4c5b6d98
agusnarvaez <agus.narvaez@outlook.com>
2022-08-24 02:46:19 -0300

    Agregué el componente general de la información del curso

--- src/sections/LearnCourse/LearnCourseInfo.js
+++ src/sections/LearnCourse/LearnCourseInfo.js
@@ -1,10 +1,25 @@
+import {useState, useEffect} from 'react'
+// Importo componentes
+import LearnCourseInfoNav from '../../components/LearnCourse/LearnCourseInfoNav'
+import LearnCourseInfoContainer from '../../components/LearnCourse/LearnCourseInfoContainer'
 
 
 function LearnCourseInfo(props) {
 	const course = props.course
+	const [infoIndex,setInfoIndex]=useState(0)
+	
+	useEffect(() => {
+
+/* 		console.log(infoIndex) */
+
+	},[infoIndex])
 	return (
 
 		<section className="learnCoursePage_Info">
+
+				<LearnCourseInfoNav infoIndex={infoIndex} setInfoIndex={setInfoIndex}/>
+				
+				<LearnCourseInfoContainer infoIndex={infoIndex} course={course} />
 				
 		</section>
 
