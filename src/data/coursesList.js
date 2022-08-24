17ccb3b25a947ff872b5cf998685cde5feb96128
agusnarvaez <agus.narvaez@outlook.com>
2022-08-24 02:45:09 -0300

    Creación de cursos

--- src/data/coursesList.js
+++ src/data/coursesList.js
@@ -3,15 +3,21 @@ const coursesList = [
 		id: 0,
 		title: '¿Cómo registrarme en Metro Futuro?',
 		cover: "/assets/images/learn/learnCoursesCardDemo.jpg",
-		description: 'Aprende como ingresar a la plataforma y registrarte para comenzar a invertir.',
+		description: 'Aprende cómo ingresar a la plataforma y registrarte para comenzar a invertir.',
 		difficulty: 'Básico',
 		videos: [
-			{
+			/* {
 				title: "Curso 1: Introducción a la plataforma",
 				video: ""
-			}
+			} */
 		],
-		author: "Juan Porrazzo"
+		author: "Juan Porrazzo",
+		aditionalContents:[{
+			title: "Como registrarse en Metro Futuro",
+			description: "Aprende cómo ingresar a la plataforma y registrarte para comenzar a invertir.",
+			type: "PDF",
+			url:""
+		}]
 	},
 	{
 		id: 1,
@@ -20,20 +26,12 @@ const coursesList = [
 		description: 'Aprende todo lo necesario para saber acerca de los nuevos términos en tecnología blockchain y ser un experto en la materia',
 		difficulty: 'Básico',
 		videos: [
-			{
+			/* {
 				title: "Curso 1: Introducción a Blockchain",
 				video: ""
-			}
+			} */
 		],
-		author: "Juan Porrazzo"
-	},
-	{
-		id: 2,
-		title: '¿Que es la tecnología bockchain?',
-		cover: "/assets/images/learn/learnCoursesCardDemo.jpg",
-		description: 'Aprende todo lo necesario para saber acerca de los nuevos términos en tecnología blockchain y ser un experto en la materia',
-		difficulty: 'Avanzado',
-		videos: [],
-		author: "Juan Porrazzo"
+		author: "Juan Porrazzo",
+		aditionalContents:[]
 	}]
 export default coursesList;
