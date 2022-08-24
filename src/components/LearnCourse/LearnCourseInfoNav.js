a170478f93e29f6495efbee66e2d7dd6baeef486
agusnarvaez <agus.narvaez@outlook.com>
2022-08-24 02:47:16 -0300

    Agregué Navegador de contenido de curso

---
+++ src/components/LearnCourse/LearnCourseInfoNav.js
@@ -0,0 +1,20 @@
+
+function LearnCourseInfoNav({infoIndex,setInfoIndex}/* handleClick */) {
+
+	const navItems = ["Descripción","Videos","Contenido Adicional"]
+
+	return (
+		<ul className="learnCoursePage_Nav">
+			{navItems.map((item,key)=>{ 
+				return(
+					<li 
+						onClick={()=>setInfoIndex(key)}
+						key={key}
+						className={infoIndex===key?"activeNav":""}>
+							{item}
+					</li>
+				)})}
+		</ul>
+	)
+}
+export default LearnCourseInfoNav;
