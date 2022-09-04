b6dd7ab22b12d0fba76b303ee0723484ee3c0433
agusnarvaez <agus.narvaez@outlook.com>
2022-09-04 18:09:05 -0300

    Agregué meta tags en la home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -50,8 +50,13 @@ export default function Home({investmentsList}) {
             
             <Helmet>
                 <title>Metro Futuro | Inversión en Real Estate</title>
-                <meta name="description" content="Enterate de las últimas novedades en Real Estate y como invertir en este fantástico mundo! " />
-                
+                <meta name="description" content="Enterate de las últimas novedades en Real Estate y como invertir en este fantástico mundo!" />
+                <meta property="og:type" content="website"/>
+                <meta property="og:title" content="Inicio"/>
+                <meta property="og:image" content="/public/assets/images/icons/metroFuturoLogoTag.png"/>
+                <meta property="og:image:alt" content="Logo Metro Futuro"/>
+                <meta property="og:description" content="Enterate de las últimas novedades en Real Estate y como invertir en este fantástico mundo!"/>
+                <meta property="og:site_name" content="Metro Futuro"/>
             </Helmet>
 
             <HomeAbout/>
