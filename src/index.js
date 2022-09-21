edd0858b9f2ba4fa70676f7c863b8f5044fdaada
agusnarvaez <agus.narvaez@outlook.com>
2022-09-21 18:58:21 -0300

    Creación de carpetas reducers y actions, importación de funciones de redux en index y creación de métodos a ejecutar en posts.js de carpeta reducers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+//* SERVICES
 import React from 'react'
 import {hydrate,render} from 'react-dom'
+
+//* SERVICIOS DE API
+import { Provider } from 'react-redux';
+import {createStore,applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+import reducers from "./reducers"
+
+//* COMPONENTS
 import App from './App'
+
+//* REVISAR QUE HACE
+const store = createStore(reducers,compose(applyMiddleware(thunk)))
+
 
 const rootElement = document.getElementById("root");
 
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+//* SERVICIO DE REDUX
+import {combineReducers} from 'redux'
+
+//* IMPORTO LOS POSTS A UTILIZAR
+import posts from './posts'
+
+export default combineReducers({
+    posts,
+})
