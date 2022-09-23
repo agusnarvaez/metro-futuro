b2e2612353877d7b4ee8bae943338d44694c6b8d
agusnarvaez <agus.narvaez@outlook.com>
2022-09-23 02:34:43 -0300

    Quité comentarios innecesarios

--- src/index.js
+++ src/index.js
@@ -3,17 +3,6 @@ import {hydrate,render} from 'react-dom'
 import App from './App'
 
 
-/* ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-); */
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-/* reportWebVitals(); */
-
 const rootElement = document.getElementById("root");
 if(rootElement.hasChildNodes()){
   hydrate(<App />,rootElement);
