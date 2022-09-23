9b576fa3035539700a795f3c5eb1f61fd6682e6d
agusnarvaez <agus.narvaez@outlook.com>
2022-09-23 01:41:09 -0300

    Corregí ubicación y url de DegradedLink en LearnCourseInfo

--- src/components/LearnCourse/LearnCourseInfoContainer.js
+++ src/components/LearnCourse/LearnCourseInfoContainer.js
@@ -1,19 +1,24 @@
-// COMPONENTS
+//* COMPONENTS
 import LearnCourseInfoDescription from "./LearnCourseInfoDescription";
 import LearnCourseInfoVideos from "./LearnCourseInfoVideos";
 import LearnCourseInfoAditional from "./LearnCourseInfoAditional";
 
-export default function LearnCourseInfoContainer({infoIndex,item,url,type}) {
+
+
+export default function LearnCourseInfoContainer({infoIndex,item}) {
 	
 	const infoContent = [
-			<LearnCourseInfoDescription content={item.content} item={item} url={url} type={type}/>,
+			<LearnCourseInfoDescription content={item.content} item={item} />,
 			<LearnCourseInfoVideos item={item}/>,
 			<LearnCourseInfoAditional item={item}/>
 		]
 
 	return (
 		<div className="learnCoursePage_InfoContainer">
+
 			{infoContent[infoIndex]}
+
+
 		</div>
 	)
 }
--- src/components/LearnCourse/LearnCourseInfoDescription.js
+++ src/components/LearnCourse/LearnCourseInfoDescription.js
@@ -5,7 +5,7 @@ import LearnCourseInfoDescriptionCard from './LearnCourseInfoDescriptionCard';
 import LearnCourseInfoDescriptionResume from './LearnCourseInfoDescriptionResume';
 import DegradedNavLink from "./../DegradedNavLink";
 
-export default function LearnCourseInfoDescription({content,item,url,type}) {
+export default function LearnCourseInfoDescription({content,item}) {
 	
 	return (
 		<div className="learnCoursePage_InfoDescription">
@@ -15,7 +15,6 @@ export default function LearnCourseInfoDescription({content,item,url,type}) {
 			{content.map((item,key) => {
 				return <LearnCourseInfoDescriptionCard key={key} content={item} />
 			})}
-			<DegradedNavLink route={`/learn/${url}`} text={`Más ${type}s`} backgroundColor="white" />
 		</div>	
 	)
 }
