2204557ba48f80c1a010afa8530c819f3be521a7
agusnarvaez <agus.narvaez@outlook.com>
2022-10-04 12:11:42 -0300

    Importé la url

--- src/sections/Learn/LearnCourses.js
+++ src/sections/Learn/LearnCourses.js
@@ -2,14 +2,11 @@
 import LearnCoursesCard from '../../components/Learn/LearnCoursesCard'
 
 function LearnCourses({list,url}) {
-
 	return (
-
 		<section id='learnCourses'>
 			{list.map((item,key) => {
 				return <LearnCoursesCard key={key} id={key} item={item.fields} url={url}/>})}
 		</section>
-
 	)
 }
 export default LearnCourses;
