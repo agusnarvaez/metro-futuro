92ef1816493fe10431f1a0324dc5dc2fcd95cb66
agusnarvaez <agus.narvaez@outlook.com>
2022-10-04 12:09:14 -0300

    Creo cliente que solicita los elementos de Contentful

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,12 @@
 import * as contentful from 'contentful'
 import credentials from '../credentials'
-const client = contentful.createClient({
+
+export const articles = contentful.createClient({
+    space: credentials.contentful.space,
+    accessToken: credentials.contentful.accessToken
+})
+
+export const courses = contentful.createClient({
     space: credentials.contentful.space,
     accessToken: credentials.contentful.accessToken
 })
