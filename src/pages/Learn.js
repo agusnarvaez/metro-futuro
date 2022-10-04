a75eb3b9a565bf0741fd8fb05ce54adeb4bb2889
agusnarvaez <agus.narvaez@outlook.com>
2022-10-04 12:05:43 -0300

    Realicé pedido a cliente de Contentful e importé la función path que solicita la ruta

--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -5,16 +5,30 @@ import LearnSlogan from"../sections/Learn/LearnSlogan.js"
 import LearnCourses from '../sections/Learn/LearnCourses.js'
 import HelmetData from "../components/HelmetData";
 
-export default function Learn({list,metaData}) {
-	const text = list.title;
+import { useEffect } from 'react';
+
+import {articles} from '../client/client'
+
+import { path } from "../Functions/learnFunctions.js";
+
+export default function Learn({metaData,list,setList}) {
+	
+	useEffect(()=>{
+		articles.getEntries()
+		.then((articles)=>{
+			const newList = articles.items.filter(article=>article.sys.contentType.sys.id===path)
+			setList(newList)
+		})
+		.catch(error=>console.log(error))
+	  },[])
+
 	return (
 		<main className='learnPage'>
 			<HelmetData metaData={metaData} />
 
-			<LearnSlogan text={text} />
+			<LearnSlogan path={path} />
 
-			<LearnCourses list={list.list} url={list.url} />
-
+			<LearnCourses list={list} url={path}  />
 		</main>
 	)
 }
