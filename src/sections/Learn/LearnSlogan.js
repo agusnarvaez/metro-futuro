41c6fa2e0eef48b16f6489494f42c8abaa9502b9
agusnarvaez <agus.narvaez@outlook.com>
2022-10-04 12:06:38 -0300

    Importé la función Path

--- src/sections/Learn/LearnSlogan.js
+++ src/sections/Learn/LearnSlogan.js
@@ -1,13 +1,13 @@
 
 import background from '../../assets/img/background/learnSloganBg.svg'
 
-export default function LearnSlogan({text}) {
+//* Importo la función que selecciona el título del Slogan
+import { selectTitle } from '../../Functions/learnFunctions';
 
+export default function LearnSlogan({path}) {
 	return (
-
 		<section id='learnSlogan'>
-			
-            <h2>{text} con <font color="#004DFF">Metro Futuro</font></h2>
+            <h2>{selectTitle(path)} con <font color="#004DFF">Metro Futuro</font></h2>
 
 			<img src={background} alt='background' className='sectionBackground' id="learnSloganBackground" />
 		</section>
