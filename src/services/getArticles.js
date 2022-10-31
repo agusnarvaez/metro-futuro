fb107f1ea965f848085bf738a6b10141e1343627
agusnarvaez <agus.narvaez@outlook.com>
2022-10-31 01:47:59 -0300

    Quité código duplicado en getArticles

--- src/services/getArticles.js
+++ src/services/getArticles.js
@@ -2,21 +2,11 @@ import {articles} from '../client/client'
 
 const reqApi = async(list,setList)=>{
     if(list.length===0){
-        const pathSplited = window.location.pathname.split('/')[1]
-
-        const contentType = ()=>{
-            switch(pathSplited){
-                case "investments": return "property"
-                case "blog": return "blog"
-                case "course": return "courses"
-                default: break
-            }
-        }
-
         const api = await articles.getEntries()
         setList(api.items)
     }
 }
+
 const filterItems = (list,setNewList)=>{
     if(list.length>0){
         const pathSplited = window.location.pathname.split('/')[1]
