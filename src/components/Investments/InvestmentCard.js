cbb97dc6e248a7d94fad37172a7bfb485f868ef0
agusnarvaez <agus.narvaez@outlook.com>
2022-11-09 23:43:08 -0300

    Agregué cartel de sold out o vigente

--- src/components/Investments/InvestmentCard.js
+++ src/components/Investments/InvestmentCard.js
@@ -9,7 +9,7 @@ import InvestmentCardInfo from "./InvestmentCardInfo"
 export default function InvestmentCard({investment,index}) {
     return (
         <article className="investmentsPageProjectCard">
-            
+            <div className={`investmentsPageProjectCardAvailability investmentsPageProjectCardAvailability--${investment.available}`}>{investment.available?"Vigente":"Sold out"}</div>
             <InvestmentCardCover cover={investment.photos[0].fields.file.url} />
                 
             <InvestmentCardInfo investment={investment} index={index}/>
--- src/sections/Investments/InvestmentCard.js
+++ src/sections/Investments/InvestmentCard.js
@@ -1,31 +1,32 @@
 
 import { NavLink } from 'react-router-dom'
 
-export default function InvestmentCard(props) {
+export default function InvestmentCard({investment,index}) {
     
     return (
         <article className="investmentsPageProjectCard">
-            <NavLink to={`/investments/${props.id}`} exact="true">
+            <NavLink to={`/investments/${investment.id}`} exact="true">
                 <div className="investmentsPageProjectsCover">
-                    <img alt="investmentsPageProjectsCover" src={props.cover}/>
+                    <img className="investmentsPageProjectsCoverImg" alt="investmentsPageProjectsCoverImg" src={investment.cover}/>
+                    
                 </div>
                 <div className="investmentsPageProjectInfo">
                     <div className="investmentsPageProjectInfoTitle">
-                        <h2>{props.title}</h2>
-                        <p className="investmentsPageProjectInfoAddress">{props.address}</p>
+                        <h2>{investment.title}</h2>
+                        <p className="investmentsPageProjectInfoAddress">{investment.address}</p>
                     </div>
                     <ul className="investmentsPageProjectInfoDetails">
                         <li>
-                            <p>Ganancia estimada</p><font color="#004DFF">{props.estimatedEarnings}</font>
+                            <p>Ganancia estimada</p><font color="#004DFF">{investment.estimatedEarnings}</font>
                         </li>
                         <li>
-                            <p>Vecimiento del contrato</p><font color="#004DFF">{props.contractExpiration}</font>
+                            <p>Vecimiento del contrato</p><font color="#004DFF">{investment.contractExpiration}</font>
                         </li>
                         <li>
-                            <p>Superficie M<sup>2</sup></p><font color="#004DFF">{props.area}</font>
+                            <p>Superficie M<sup>2</sup></p><font color="#004DFF">{investment.area}</font>
                         </li>
                         <li>
-                            <p>Valor M<sup>2</sup></p><font color="#004DFF">{props.areaValue}</font>
+                            <p>Valor M<sup>2</sup></p><font color="#004DFF">{investment.areaValue}</font>
                         </li>
                     </ul>
                 </div>
