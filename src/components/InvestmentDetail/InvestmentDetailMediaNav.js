8f0963f19bfd86222ab4bb7355d5ae7b348fed09
agusnarvaez <agus.narvaez@outlook.com>
2022-11-09 23:11:18 -0300

    Cambié ícono de InvestmentDetailMedia

--- src/components/InvestmentDetail/InvestmentDetailMediaNav.js
+++ src/components/InvestmentDetail/InvestmentDetailMediaNav.js
@@ -2,6 +2,7 @@
 import investmentDetailMediaImage from "../../assets/img/icons/investmentDetailMediaImage.svg"
 import investmentDetailMediaVideo from "../../assets/img/icons/investmentDetailMediaVideo.svg"
 import investmentDetailMedia360 from "../../assets/img/icons/investmentDetailMedia360.svg"
+import investmentDetailMediaBlueprints from "../../assets/img/icons/investmentDetailMediaBlueprints.svg"
 export default function InvestmentDetailMediaNav({index,setIndex}) {
 
     return (
@@ -12,7 +13,7 @@ export default function InvestmentDetailMediaNav({index,setIndex}) {
             <li className="investmentDetailMediaNavSeparator"></li>
             <li onClick={()=>setIndex(2)} className={index===2?"investmentDetailMediaNav--active":0}><img src={investmentDetailMedia360} alt="icono" />360</li>
             <li className="investmentDetailMediaNavSeparator"></li>
-            <li onClick={()=>setIndex(3)} className={index===3?"investmentDetailMediaNav--active":0}><img src={investmentDetailMedia360} alt="icono" />Planos</li>
+            <li onClick={()=>setIndex(3)} className={index===3?"investmentDetailMediaNav--active":0}><img src={investmentDetailMediaBlueprints} alt="icono" />Planos</li>
         </ul>
     )
 }
---
+++ src/sections/Investments/InvestmentCard.js
@@ -0,0 +1,35 @@
+
+import { NavLink } from 'react-router-dom'
+
+export default function InvestmentCard(props) {
+    
+    return (
+        <article className="investmentsPageProjectCard">
+            <NavLink to={`/investments/${props.id}`} exact="true">
+                <div className="investmentsPageProjectsCover">
+                    <img alt="investmentsPageProjectsCover" src={props.cover}/>
+                </div>
+                <div className="investmentsPageProjectInfo">
+                    <div className="investmentsPageProjectInfoTitle">
+                        <h2>{props.title}</h2>
+                        <p className="investmentsPageProjectInfoAddress">{props.address}</p>
+                    </div>
+                    <ul className="investmentsPageProjectInfoDetails">
+                        <li>
+                            <p>Ganancia estimada</p><font color="#004DFF">{props.estimatedEarnings}</font>
+                        </li>
+                        <li>
+                            <p>Vecimiento del contrato</p><font color="#004DFF">{props.contractExpiration}</font>
+                        </li>
+                        <li>
+                            <p>Superficie M<sup>2</sup></p><font color="#004DFF">{props.area}</font>
+                        </li>
+                        <li>
+                            <p>Valor M<sup>2</sup></p><font color="#004DFF">{props.areaValue}</font>
+                        </li>
+                    </ul>
+                </div>
+            </NavLink> 
+        </article>
+    )
+}
