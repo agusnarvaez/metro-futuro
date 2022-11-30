3c1b6254787d91bdb7791d9c8e09000e45dedb6c
agusnarvaez <agus.narvaez@outlook.com>
2022-11-30 03:01:20 -0300

    Reestructuré formulario utilizando useRef

--- src/components/Contact/LargeInput.js
+++ src/components/Contact/LargeInput.js
@@ -1,17 +1,45 @@
-export default function LargeInput({field,handleChange}) {
+import { useState } from "react"
+export default function LargeInput({field,contact,setContact}) {
+    const [inputClass,setInputClass] = useState("largeInput")
+
+    const handleChange = (event) => {
+
+        const { name, value } = event.target
+        setContact({ ...contact, [name]: value })
+
+        if(event.target.value.length > 0){
+            field.isValid = true
+        }else{
+            field.isValid = false
+        }
+        console.log(contact)
+    }
+
+    const handleBlur = () => {
+        if(field.isValid){
+            setInputClass("largeInput input--valid")
+            field.showErrors = false
+        }else{
+            setInputClass("largeInput input--invalid")
+            field.showErrors = true
+        }
+    }
+
     return (
-        <div className={field.class} id="inputContainer">
-                        <textarea
-                            className={field.class}
-                            id="input"
-                            name={field.name}
-                            placeholder={field.placeholder}
-                            value={field.value}
-                            onChange={handleChange}
-                            onFocus={()=>{field.container= "inputLargeContainer inputContainerFocus"}}
-                            onBlur={()=>{field.container= "inputLargeContainer"}}
-                            required
-                        />
-        </div>
+        <>
+            <div className={inputClass} id="inputContainer">
+                <textarea
+                    className={field.class}
+                    id="input"
+                    name={field.name}
+                    placeholder={field.placeholder}
+                    onChange={handleChange}
+                    onFocus={()=>{setInputClass("largeInput input--focus")}}
+                    onBlur={handleBlur}
+                    required
+                />
+            </div>
+            {field.showErrors?<p className="errorShown">{field.error}</p>:null}
+        </>
     )
 }
--- src/components/Contact/SmallInput.js
+++ src/components/Contact/SmallInput.js
@@ -1,18 +1,51 @@
-export default function SmallInput({field,handleChange}) {
+import { useState } from "react"
+export default function SmallInput({field,contact,setContact}) {
+
+    const [inputClass,setInputClass] = useState("inputContainer")
+    const checkEmail = (email)=>{
+        if(email.includes("@")){
+            field.isValid = true
+        }else{
+            field.isValid = false
+        }
+    }
+    const handleChange = (event) => {
+
+        const { name, value } = event.target
+        setContact({ ...contact, [name]: value })
+
+        event.target.value.length > 0 ?
+            (field.name === "from_email") ?
+                checkEmail(event.target.value)
+            : field.isValid = true
+        : field.isValid = false
+    }
+
+    const handleBlur = () => {
+        if(field.isValid){
+            setInputClass("inputContainer input--valid")
+            field.showErrors = false
+        }else{
+            setInputClass("inputContainer input--invalid")
+            field.showErrors = true
+        }
+    }
     return (
-        <div className={field.type} id={field.type}>
-            <input
-                className={field.class}
-                id="input"
-                name={field.name}
-                placeholder={field.placeholder}
-                type="text"
-                value={field.value}
-                onChange={handleChange}
-                onFocus={()=>{field.container=`${field.type} ${field.type}Focus`}}
-                onBlur={()=>{field.container= field.type}}
-                required
-            />
-        </div>
+        <>
+            <div className={inputClass} id={field.type}>
+                <input
+                    className={field.class}
+                    id="input"
+                    name={field.name}
+                    placeholder={field.placeholder}
+                    type="text"
+                    onChange={handleChange}
+                    onFocus={()=>{setInputClass("inputContainer input--focus")}}
+                    onBlur={handleBlur}
+                    required
+                />
+            </div>
+            {field.showErrors?<p className="errorShown">{field.error}</p>:null}
+        </>
     )
 }
