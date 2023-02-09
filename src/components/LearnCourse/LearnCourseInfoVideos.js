4b855c5e5729d542ad20b7d816869ca51fab4f6c
agusnarvaez <agus.narvaez@outlook.com>
2023-02-09 14:43:01 -0300

    Comenté console.log

--- src/components/LearnCourse/LearnCourseInfoVideos.js
+++ src/components/LearnCourse/LearnCourseInfoVideos.js
@@ -2,30 +2,30 @@ import {useState,useEffect} from 'react'
 import LearnCourseInfoVideoCard from "./LearnCourseInfoVideoCard";
 
 export default function LearnCourseInfoVideos({item}){
-	
-	
+
+
 	const [videos,setVideos]=useState([])
-	
+
 	useEffect(() => {
 		if(item!==undefined){
-			console.log(item.youTubeVideosTitles)
+			
 			if(item.youTubeVideosTitles.length===0){
 				setVideos(<p>Aún no se encuentran videos disponibles</p>)
 			}else{
-				setVideos(item.youTubeVideosTitles.map((videoTitle,key)=>{ 
+				setVideos(item.youTubeVideosTitles.map((videoTitle,key)=>{
 					return(
 						<LearnCourseInfoVideoCard title={videoTitle} link={item.youTubeLinks[key]} />
 					)})
 				)
 			}
 		}
-		
-		
+
+
 	},[item])
 
 	return (
 		<div className="learnCoursePage_InfoVideos">
 			{videos}
-		</div>	
+		</div>
 	)
 }
