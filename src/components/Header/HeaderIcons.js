277d3b6647a22c80f2390d65f4fe0b7ce0e61771
agusnarvaez <agus.narvaez@outlook.com>
2023-02-09 22:17:11 -0300

    Puse el ícono de metro futuro como Home y agregué cambios pedidos por Juan

--- src/components/Header/HeaderIcons.js
+++ src/components/Header/HeaderIcons.js
@@ -5,16 +5,31 @@ import metroFuturoLogo from "../../assets/img/icons/metroFuturoLogo.png"
 import burgerMenuIcon from "../../assets/img/icons/burgerMenuIcon.png";
 import burgerMenuCrossIcon from "../../assets/img/icons/BurgerMenuCrossIcon.png";
 
+import { NavLink } from 'react-router-dom'
+
 export default function HeaderIcons({burgerMenu,setBurgerMenu}) {
-    
+    const scrollToTop = () =>{
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      };
     return (
 
             <div className="headerIcons">
-                <img
+                <NavLink
+                    onClick={scrollToTop}
+                    className={({ isActive }) => (isActive ? "navLink active" : "navLink")}
+                    to="/"
+                    exact="true"
+                >
+                    <img
                     src={metroFuturoLogo}
                     alt="metroFuturoLogo"
                     className="metroFuturoLogo"
                 />
+                </NavLink>
+
                 {/** Burger Menu Icons **/}
                 <img
                     className={burgerMenu?"burgerMenuIcon":"burgerMenuIconHidden"}
--- src/sections/Home/HomeOportunity.js
+++ src/sections/Home/HomeOportunity.js
@@ -8,13 +8,13 @@ import DegradedNavLink from "../../components/DegradedNavLink";
 //* Íconos
 
 export default function HomeOportunity({principalInvestment}) {
-    
+
     return (
         <section className="sectionInfo" id="homeOportunity">
             <img src={principalInvestment.cover} alt="investmentCover" className='sectionCover'/>
             <div className='sectionInfo'>
 
-                <h2 className='section_subTitle'>Oportunidad de inversión inmobiliaria en <span className="degradedText">{principalInvestment.city +", "+ principalInvestment.country}</span></h2>
+                <h2 className='section_subTitle'>Invertí en inmuebles tokenixados en <span className="degradedText">{principalInvestment.country}</span></h2>
 
                 <p className="section_paragraph">Rentabilidad neta estimada anual {principalInvestment.estimatedEarnings}</p>
 
