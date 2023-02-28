1c75c22cd139ab5f0bbdf1bf38fa3eea83aac0be
Agus Narvaez <agus.narvaez@outlook.com>
2023-02-27 23:04:09 -0300

    Agregué link de quiero invertir en título de inversión

--- src/sections/InvestmentDetail/InvestmentDetailTitle.js
+++ src/sections/InvestmentDetail/InvestmentDetailTitle.js
@@ -1,13 +1,15 @@
+import DegradedLink from "../../components/DegradedLink";
 
 export default function InvestmentDetailTitle({investment}) {
     const address = investment.street + ", " + investment.city + ", " + investment.country;
-
+    const degLinkId="degradedLink--inverted"
     return (
         <section className="investmentDetailTitle">
             <div className="investmentDetailTitleInfo">
                 <h1>{investment.title}</h1>
                 <h2>{address}</h2>
             </div>
+            <DegradedLink  text="¡Quiero invertir!" route={investment.platformLink} backgroundColor="" id={degLinkId} /* onFocus={changeDegLink} onBlur={changeDegLink} *//>
             {/*  <div className="investmentDetailPageTitleShare">
 
             </div> */}
