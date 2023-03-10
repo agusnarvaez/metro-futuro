a3d442624020d83d18fcab1e5af7f2d70bbb4241
Agus Narvaez <agus.narvaez@outlook.com>
2023-03-10 02:03:25 -0300

    Agregué url's cannonical a metaData

--- src/data/metaData.js
+++ src/data/metaData.js
@@ -4,67 +4,78 @@ const metaData = {
         {
             title: "Inversión en Real Estate",
             description:"Enterate de las últimas novedades en Real Estate y como invertir en este fantástico mundo!",
-            keywords:"inversiones, real, estate, españa, token, blockchain, metro futuro"
+            keywords:"inversiones, real, estate, españa, token, blockchain, metro futuro",
+            cannonical: "/"
         },
     investments:
         {
             title: "Inmuebles",
             description:"Invertí en inmuebles tokenizados en europa",
-            keywords:"inversión, novedades, real estate"
+            keywords:"inversión, novedades, real estate",
+            cannonical: "/inmuebles"
         },
     investmentDetail:
         {
             title: "",
             description:"",
-            keywords:"inversión, novedades, real estate"
+            keywords:"inversión, novedades, real estate",
+            cannonical: "/inmuebles/0"
         },
     about:
         {
             title: "Conocé el equipo",
             description:"Conocé nuestra historia y fundamentos que forjan nuestro éxito y confiabilidad",
-            keywords:"sobre nosotros, equipo, foco, servicios, inversiones, misión, visión, valores"
+            keywords:"sobre nosotros, equipo, foco, servicios, inversiones, misión, visión, valores",
+            cannonical: "/sobreNosotros"
         },
     contact:
         {
             title: "Contacto",
             description:"Realizá tu consulta al equipo para guiarte en el mundo de las inversiones tokenizadas",
-            keywords:"contacto, consulta, inversiones"
+            keywords:"contacto, consulta, inversiones",
+            cannonical: "/contacto"
         },
     faq:
         {
             title: "Preguntas frecuentes",
             description:"Descubre si algunas de tus dudas están compartidas por las de la comunidad",
-            keywords:"faq, preguntas frecuentes, inversiones"
+            keywords:"faq, preguntas frecuentes, inversiones",
+            cannonical: "/preguntasFrecuentes"
         },
     blog:
         {
             title: "Infórmate",
             description:"Descubre los últimos datos y novedades del Real Estate",
-            keywords:"blog, novedades, real estate"
+            keywords:"blog, novedades, real estate",
+            cannonical: "/blog"
         },
     blogArticle:
         {
             title: "",
             description:"",
-            keywords:"blog, artículo, novedad, real estate"
+            keywords:"blog, artículo, novedad, real estate",
+            cannonical: "/blog/0"
         },
     courses:
         {
             title: "Aprende",
             description:"Descubre la historia y los mecanismos para lograr tus objetivos económicos con el blockchain",
-            keywords:"cursos, novedades, real estate"
+            keywords:"cursos, novedades, real estate",
+            cannonical: "/aprende"
         },
     course:
         {
             title: "",
             description:"",
-            keywords:"curso, artículo, novedad, real estate"
+            keywords:"curso, artículo, novedad, real estate",
+            cannonical: "/aprende/0"
         },
     error404:
         {
             title: "Página no encontrada | Error 404",
             description:"No se ha encontrado la página que busca. Puede volver al inicio.",
-            keywords:"error, 404"
+            keywords:"error, 404",
+            cannonical: "/*"
         }
 };
 export default metaData;
