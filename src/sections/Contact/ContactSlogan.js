65c1d21f32e80fcb82e995804d4add8fffad4049
Agus Narvaez <agus.narvaez@outlook.com>
2023-03-21 20:51:38 -0300

    Corrección etiquetas Contact

--- src/sections/Contact/ContactSlogan.js
+++ src/sections/Contact/ContactSlogan.js
@@ -2,7 +2,7 @@ export default function ContactSlogan() {
     return (
         <section id="contactPageSlogan">
             <h1>¡Estamos para <br/><font color="#004DFF">ayudarte</font>!</h1>
-            <p>Ponte en contacto con nosotros enviándonos un mensaje</p>
+            <h2>Ponte en contacto con nosotros enviándonos un mensaje</h2>
         </section>
     )
 }
