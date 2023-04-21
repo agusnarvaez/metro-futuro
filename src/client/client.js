0dff80d546c34be5258f124336c9a68bbbb5bdaf
Agus Narvaez <agus.narvaez@outlook.com>
2023-04-21 00:24:44 -0300

    Muevo funcion reqApi a client.js

--- src/client/client.js
+++ src/client/client.js
@@ -1,7 +1,20 @@
 import {createClient} from 'contentful'
 import credentials from '../credentials'
-
+import {emptyList} from '../utils/listFunctions'
 export const articles = createClient({
     space: credentials.contentful.space,
     accessToken: credentials.contentful.accessToken
 })
+
+export const reqApi = async(list,setList)=>{
+    if(emptyList(list)){
+        try{
+            const api = await articles.getEntries()
+            await setList(api.items)
+            
+        }catch(err){
+            console.log("Error en la petición")
+            console.log(err)
+        }
+    }
+}
