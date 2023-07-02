2af8bc624acf0c741110613884dfe30c82518865
agusnarvaez <agus.narvaez@outlook.com>
2023-07-02 17:04:21 -0300

    Saco /index

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -11,15 +11,15 @@ export default function Error404({metaData}) {
     return (
         <main className="NotFound404ErrorPage" id="NotFound404ErrorPage">
             <HelmetData metaData={metaData} />
-            
+
             <img src={NotFound404ErrorImage} alt='Imagen de error 404' title="Imagen de error 404" loading="lazy"/>
 
             <div className="sectionInfo">
-                
+
                 <h1>¡Lo sentimos! La página que estás buscando no pudo ser encontrada</h1>
-                
-                <DegradedNavLink route="/inicio" text="Volver a la Home" backgroundColor="white" id="404ErrorLinkToHome"/>
-                
+
+                <DegradedNavLink route="/" text="Volver a la Home" backgroundColor="white" id="404ErrorLinkToHome"/>
+
                 <DegradedNavLink route="/contacto" text="Contáctanos" backgroundColor="white" id="404ErrorLinkToContact"/>
             </div>
         </main>
