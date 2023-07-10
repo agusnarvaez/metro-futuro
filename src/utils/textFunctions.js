83a0a944c4981ab79f433398272f62fec2497920
agusnarvaez <agus.narvaez@outlook.com>
2023-07-10 19:00:26 -0300

    Corrección de estilos de curso

--- src/utils/textFunctions.js
+++ src/utils/textFunctions.js
@@ -3,7 +3,7 @@ import {BLOCKS,INLINES} from "@contentful/rich-text-types"
 const RICHTEXT_OPTIONS = {
 	renderNode:{
 		[BLOCKS.PARAGRAPH]:(node,children)=>{
-			return <p className="py-2">{children}</p>
+			return <p>{children}</p>
 		},
 		[INLINES.HYPERLINK]:(node,children)=>{
 			return <a href={node.data.uri} title={children}>{children}</a>
@@ -17,20 +17,7 @@ const RICHTEXT_OPTIONS = {
 		[BLOCKS.QUOTE]: (node, children) => {
 		return <blockquote>{children}</blockquote>;
 		}
-	},
-	renderText: (text) => {
-		return text.split('\n').flatMap((textChunk, index) => {
-		  if (index > 0) {
-			return [<br key={index} />, textChunk];
-		  }
-		  return textChunk.split('**').map((chunk, i) => {
-			if (i % 2 === 1) {
-			  return <strong key={i}>{chunk}</strong>;
-			}
-			return chunk;
-		  });
-		});
-	  },
+	}
 }
 
 export {RICHTEXT_OPTIONS}
