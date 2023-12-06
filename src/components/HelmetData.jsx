26b2bbea5eaf71f9f10e5a2d22b938bedc57e482
agusnarvaez <agus.narvaez@outlook.com>
2023-12-06 16:25:18 -0300

    Agrego condicioanal de etiquetas meta en HelmetData

--- src/components/HelmetData.jsx
+++ src/components/HelmetData.jsx
@@ -3,19 +3,20 @@
 import { Helmet,HelmetProvider } from 'react-helmet-async';
 
 export default function HelmetData({metaData}) {
+    const isFound = metaData.title !== "Página no encontrada | Error 404"
 
     return (
         <HelmetProvider>
             <Helmet defer={true}>
                 <title>Metro Futuro | {metaData.title}</title>
-
-                <meta name="description" content={metaData.description} />
-                <meta name="keywords" content={metaData.keywords} />
-
-
                 <meta property="og:title" content={`Metro Futuro | ${metaData.title}`}/>
-                <meta property="og:description" content={metaData.description}/>
-                <link rel="canonical" href={metaData.cannonical}/>
+
+                {isFound &&
+                    <meta name="keywords" content={metaData.keywords} /> &&
+                    <meta name="description" content={metaData.description} /> &&
+                    <meta property="og:description" content={metaData.description}/> &&
+                    <link rel="canonical" href={metaData.cannonical}/>
+                }
             </Helmet>
         </HelmetProvider>
     )
