c057cec115bccd309f30b32e932f7d3338468f95
agusnarvaez <agus.narvaez@outlook.com>
2024-01-12 12:33:42 -0300

    Corrijo la forma en la que se aplica la indexación en meta

--- src/components/HelmetData.jsx
+++ src/components/HelmetData.jsx
@@ -4,19 +4,17 @@ import { Helmet,HelmetProvider } from 'react-helmet-async'
 
 export default function HelmetData({metaData}) {
     const isFound = metaData.title !== "Página no encontrada | Error 404"
-
     return (
         <HelmetProvider>
-            <Helmet defer={true}>
+            <Helmet /* defer={true} */>
                 <title>{metaData.title}</title>
-                <meta property="og:title" content={`Metro Futuro | ${metaData.title}`}/>
+                <meta property="og:title" content={`${metaData.title}`}/>
 
-                {isFound &&
-                    <meta name="keywords" content={metaData.keywords} /> &&
-                    <meta name="description" content={metaData.description} /> &&
-                    <meta property="og:description" content={metaData.description}/> &&
-                    <link rel="canonical" href={metaData.cannonical}/>
-                }
+                {!isFound && <meta name="robots" content="noindex" /> }
+                <meta name="keywords" content={metaData.keywords} />
+                <meta name="description" content={metaData.description} />
+                <meta property="og:description" content={metaData.description} />
+                <link rel="canonical" href={metaData.canonical} />
             </Helmet>
         </HelmetProvider>
     )
