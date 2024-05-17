08205fa870a9501f3928d84196815bf463785630
agusnarvaez <agus.narvaez@outlook.com>
2024-05-17 12:41:47 -0300

    Saco botones innecesarios en investment

--- src/sections/InvestmentDetail/InvestmentDetailDownloads.jsx
+++ src/sections/InvestmentDetail/InvestmentDetailDownloads.jsx
@@ -2,16 +2,16 @@
 import DegradedLink from "../../components/DegradedNavLink"
 
 export default function InvestmentDetailDownloads({investment}) {
-    
+
     const list = [
-        {
+/*         {
             url:investment.whitePaper?investment.whitePaper.fields.file.url:"",
             text: "Descargar White Paper"
         },
         {
             url: investment.contract?investment.contract.fields.file.url:"",
             text: "Descargar contrato"
-        },
+        }, */
         {
             url: investment.brochure?investment.brochure.fields.file.url:"",
             text: "Descargar brochure"
