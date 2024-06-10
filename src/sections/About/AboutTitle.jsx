d3eb35d999ba348666c935a6f30510fd6c2e2441
agusnarvaez <agus.narvaez@outlook.com>
2024-06-09 23:32:40 -0300

    Corrijo logo en Banner de sección About

--- src/sections/About/AboutTitle.jsx
+++ src/sections/About/AboutTitle.jsx
@@ -2,17 +2,17 @@
 import aboutTitleBg from "../../assets/img/background/aboutTitleBg.svg"
 
 // Logos
-import metroFuturoLogo from "../../assets/img/logos/metroFuturoLogoTag.png"
+import metroFuturoLogo from "../../assets/img/logos/metroFuturoLogo.png"
 
 
 export default function AboutTitle() {
 
     return (
         <section id="aboutTitle">
-            <img className="aboutMetroLogo" alt="aboutMetroLogo" title="aboutMetroLogo" src={metroFuturoLogo} loading="lazy" />
             <h1>
                 Somos <br />
-                <font color="#004DFF">Metro Futuro</font>
+                <img className="aboutMetroLogo" alt="aboutMetroLogo" title="aboutMetroLogo" src={metroFuturoLogo} loading="lazy" />
+                {/* <font color="#004DFF">Metro Futuro</font> */}
             </h1>
             <p className='section_paragraph'>Una empresa global de inmuebles tokenizados</p>
             <img alt="aboutTitleBackground" title="aboutTitleBackground" className="sectionBackground" src={aboutTitleBg} loading="lazy" />
