ceab599bad16da5710613f667ae754b8b277dd6b
agusnarvaez <agus.narvaez@outlook.com>
2024-07-10 00:23:39 -0300

    Agrego FormButton y lógica a PopUp

--- src/sections/Learn/LearnPopUp.jsx
+++ src/sections/Learn/LearnPopUp.jsx
@@ -1,34 +1,45 @@
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import Cookies from 'js-cookie'
 import credentials from '../../credentials'
 import { Link } from 'react-router-dom'
+import FormButton from '../../components/Contact/FormButton'
 //* Importo la función que selecciona el título del Slogan
 
 export default function LearnSlogan({path,setShowPopUp}) {
 	const { register, handleSubmit, formState: { errors } } = useForm()
 
-  const onSubmit = async (data) => {
-    try {
-      const response = await fetch(`${credentials.metroFuturoApi}/users`, { // Reemplaza con la URL de tu API
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      })
-      /* const responseData = await response.json() */
-      if (response.status>=200) {
-        Cookies.set('isRegistered', 'true', { expires: 30 }) // La cookie expira en 7 días
-        setShowPopUp(false) // Ocultar el PopUp después de registrar
-      } else {
-        // Manejar la respuesta no exitosa
-		console.log(response.status)
-        console.log('Registro fallido')
-      }
-    } catch (error) {
-      console.error('Error al enviar el formulario:', error)
-    }
-  }
+	const [buttonClass, setButtonClass] = useState('contactSubmit degradedLink')
+
+	const onSubmit = async (data) => {
+		try {
+			setButtonClass('contactSubmit contactSubmit--loading')
+			const response = await fetch(
+				`${credentials.metroFuturoApi}/users`, // URL de la API
+				{
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify(data),
+				}
+			)
+
+			if (response.status>=200) {
+				Cookies.set('isRegistered', 'true', { expires: 60 })
+				setButtonClass('contactSubmit degradedLink contactSubmit--success')
+
+				setShowPopUp(false) // Ocultar el PopUp después de registrar
+			} else {
+				// Manejar la respuesta no exitosa
+				console.error('Registro fallido: ',response)
+				setButtonClass('contactSubmit contactSubmit--error')
+		}
+		} catch (error) {
+		console.error('Error al enviar el formulario:', error)
+		setButtonClass('contactSubmit contactSubmit--error')
+		}
+	}
 	return (
 		<section id='learnPopUp'>
             <h2>Completa unos pocos datos para acceder a nuestra academia de recursos educativos</h2>
@@ -71,11 +82,12 @@ export default function LearnSlogan({path,setShowPopUp}) {
 					/>
 					{errors.learnCookies && <span className="error">Debe aceptar las cookies para poder ingresar. <Link to='/cookie-politics'>¿Porque?</Link></span>}
 				</div>
-				<button className='degradedLink' type='submit'>
+				{/* <button className='degradedLink' type='submit'>
 					<div className='degradedLink_TextContainer degradedLink_TextContainer--white'>
 						<span className='degradedLink_Text'>Enviar</span>
 					</div>
-				</button>
+				</button> */}
+				<FormButton buttonClass={buttonClass} setButtonClass={setButtonClass} />
 			</form>
 		</section>
 	)
